94d969ba2395d7666f2d7f6768b76f1b44892aed
김준호 <wnsgh_s2@naver.com>
2025-07-14 16:51:29 +0900

    구조 초기화

--- wellb-front/src/main/java/kr/co/misoinfo/wellb/front/app/user/controller/auth/AuthController.java
+++ wellb-front/src/main/java/kr/co/misoinfo/wellb/front/app/user/controller/auth/AuthController.java
@@ -2,12 +2,16 @@ package kr.co.misoinfo.wellb.front.app.user.controller.auth;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class AuthController {
     @GetMapping("/user/auth/login")
     public String login() {
         return "user/auth/login";
+    }
+
+    @GetMapping("/user/auth/signup")
+    public String signIn() {
+        return "user/auth/signup";
     }
 }
